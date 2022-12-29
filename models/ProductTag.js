2b9e205fdd22ec3a3da8dabf3bb5bfea3664573b
Kevin Cain <kcain72@gmail.com>
2022-12-29 13:09:42 -0800

    last of the code

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
+ProductTag.init({
+    // Define columns
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'product',
+            key: 'id'
+        }
+    },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'tag',
+            key: 'id'
+        }
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
-);
+});
 
 module.exports = ProductTag;
